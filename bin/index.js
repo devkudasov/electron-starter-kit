ba9d9fe468a9900ebc4a075436c42bfeb03c4188
Kudasov Dmitriy <dev.kudasov@gmail.com>
2019-03-19 14:54:29 +0300

    Add basic parameters check

---
+++ bin/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const appTypes = require('./types.json');
+
+const appName = process.argv[2];
+const appType = process.argv[3];
+
+if (!appName || !appType) {
+  console.error('Please provide valide command: est <appName> --<appType>');
+}
+
+console.log(process.argv);
